2a770160eafbd4139627c9d4a61956dd7e0bddc9
nicolás <nicolas.bernardelli@gmail.com>
2022-03-31 00:10:55 -0300

    Rm Bootstrap, Add Material UI

--- myecommerce_bernardelli/src/Components/NavBar.jsx
+++ myecommerce_bernardelli/src/Components/NavBar.jsx
@@ -1,28 +1,113 @@
-import React from "react";
-import { Navbar, Container, Nav } from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.css";
-import logo from "../logo.png";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem";
 import CartWidget from "./CartWidget";
+import logo from "../logo.png";
+
+const pages = ["Vehículos", "Electrónica", "Libros"];
+
 
 export default function NavBar() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  //const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  /*const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };*/
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  /*const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };*/
+
   return (
-    <>
-      <Navbar bg="secondary" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">
-            <img alt="Logo" src={logo} width="10%" height="10%" />{" "}
-          </Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#vehiculos">Vehículos</Nav.Link>
-            <Nav.Link href="#electronica">Electronica</Nav.Link>
-            <Nav.Link href="#libros">Libros</Nav.Link>
-            <Nav.Link>
-              <CartWidget quantity={4} />{" "}
-              {/* Pongo una cantidad fija simplemente para probar, este valor será variable */}
-            </Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-    </>
+    <AppBar position="static" style ={{backgroundColor:"#FFC300"}}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
+          >
+            <img alt="Logo" src={logo} width="10%" height="10%" />
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" }, color: "black"}}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
+            align="center"
+          >
+            <img alt="Logo" src={logo} width="10%" height="10%" />
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: "black", display: "block" }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+          <Box sx={{ flexGrow: 0, color: "black"}}>
+            <CartWidget quantity={10} />
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 }
