26ef8612e6f3afff668788e05206b54eb6ae6e78
nicolás <nicolas.bernardelli@gmail.com>
2022-04-03 18:39:56 -0300

    Modify NavBar, apply bootstrap

--- myecommerce_bernardelli/src/Components/NavBar.jsx
+++ myecommerce_bernardelli/src/Components/NavBar.jsx
@@ -1,113 +1,25 @@
-import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
-import Container from "@mui/material/Container";
-import Button from "@mui/material/Button";
-import MenuItem from "@mui/material/MenuItem";
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
 import CartWidget from "./CartWidget";
 import logo from "../logo.png";
 
-const pages = ["Vehículos", "Electrónica", "Libros"];
-
-
-export default function NavBar({quantityProduct}) {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-  //const [anchorElUser, setAnchorElUser] = React.useState(null);
-
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  /*const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };*/
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  /*const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };*/
-
+export default function NavBar({ quantityProduct }) {
   return (
-    <AppBar position="static" style ={{backgroundColor:"#FFC300"}}>
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
-          >
+    <>
+      <Navbar bg="warning">
+        <Container>
+          <Navbar.Brand href="#home">
             <img alt="Logo" src={logo} width="10%" height="10%" />
-          </Typography>
-
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" }, color: "black"}}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: "block", md: "none" },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
-            align="center"
-          >
-            <img alt="Logo" src={logo} width="10%" height="10%" />
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: "black", display: "block" }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box>
-          <Box sx={{ flexGrow: 0, color: "black"}}>
+          </Navbar.Brand>
+          <Nav className="me-auto" variant="black">
+            <Nav.Link href="#vehículos">Vehículos</Nav.Link>
+            <Nav.Link href="#electrónica">Electrónica</Nav.Link>
+            <Nav.Link href="#libros">Libros</Nav.Link>
+          </Nav>
             <CartWidget quantity={quantityProduct} />
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+        </Container>
+      </Navbar>
+      ;
+    </>
   );
 }
